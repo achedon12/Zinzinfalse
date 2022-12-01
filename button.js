3b5b753118f3bc5daeac117addc92385c87bfb89
Quentin <quentin.farret@gmail.com>
2022-12-01 20:55:27 +0100

    button placed

---
+++ button.js
@@ -0,0 +1,5 @@
+button1 = document.createElement('button');
+button1.innerHTML = "test";
+console.log("test");
+button1.id = "btn1"
+document.getElementById('oui').appendChild(button1);
--- room.php
+++ room.php
@@ -6,11 +6,12 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+
     <title>Escape the Room</title>
 </head>
-<body>
-
+<body class="container" id="oui">
 <img src="Image/room.png">
-
 </body>
+<script type="text/javascript" src="button.js"></script>
+<link rel="stylesheet" href="escape.css">
 </html>
