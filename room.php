92fc9a4ab4f5da12a3bf3015d364aa01c2576993
Quentin <quentin.farret@gmail.com>
2022-12-01 18:52:15 +0100

    Initial commit for feature Escape Game

---
+++ room.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Escape the Room</title>
+</head>
+<body>
+
+<img src="Image/room.png">
+
+</body>
+</html>
