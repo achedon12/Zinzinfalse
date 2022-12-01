504250a1caf2ad94501971d75d5667da28a619e6
ferrarau <aurelien.ferrari@etu.univ-grenoble-alpes.fr>
2022-12-01 22:14:49 +0100

    entrer

---
+++ entrer.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Escape the Room</title>
+</head>
+<body class="container" id="oui">
+<img src="Image/entrer.png" alt="feur">
+<button id="un"> test </button>
+<link rel="stylesheet" href="entrer.css">
+</html>
