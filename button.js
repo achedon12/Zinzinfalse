44710bcb51b358fa77dd0ea3f524e88b81661a1d
Quentin <quentin.farret@gmail.com>
2022-12-01 23:06:30 +0100

    escape game room2 fini

--- button.js
+++ button.js
@@ -1,5 +1,50 @@
-button1 = document.createElement('button');
-button1.innerHTML = "test";
-console.log("test");
-button1.id = "btn1"
-document.getElementById('oui').appendChild(button1);
+let btn1 = document.getElementById("un");
+let btn2 = document.getElementById("deux");
+let btn3 = document.getElementById("quatre");
+btn1.addEventListener("click", alert1);
+btn2.addEventListener("click", alert2);
+btn3.addEventListener("click", alert3);
+
+
+function alert1(){
+    hideAll();
+    document.getElementById("pmod").textContent = "1234";
+    document.getElementById('modal').style.display = 'block';
+    document.getElementById("modal-close").addEventListener("click", fermer);
+}
+
+function hideAll(){
+    document.getElementById("un").hidden = true;
+    document.getElementById("deux").hidden = true;
+    document.getElementById("quatre").hidden = true;
+}
+
+function alert2(){
+    hideAll();
+    document.getElementById("pmod").textContent = "easter egg (trouver quoi)";
+    document.getElementById('modal').style.display = 'block';
+    document.getElementById("modal-close").addEventListener("click", fermer);
+}
+
+function alert3(){
+    hideAll();
+    input = document.createElement("input");
+    input.type = 'text';
+    valid = document.createElement("input");
+    valid.type = 'submit';
+    valid.value = 'Valider';
+    document.getElementById("pmod").append(input);
+    document.getElementById("pmod").append(valid);
+    document.getElementById('modal').style.display = 'block';
+    document.getElementById("modal-close").addEventListener("click", fermer);
+}
+
+function fermer(){
+    document.getElementById("pmod").textContent = "";
+    document.getElementById("modal").style.display = 'none';
+    document.getElementById("un").hidden = false;
+    document.getElementById("deux").hidden = false;
+    document.getElementById("quatre").hidden = false;
+    document.getElementById("pmod").removeChild(input);
+}
+
