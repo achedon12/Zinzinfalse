9c760e3ea231a3197567d2f5a977d7ba235ef7c2
lopi555 <theo1707@free.fr>
2022-12-02 01:47:50 +0100

    add score

--- pagechoice.php
+++ pagechoice.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
+<?php
+session_start();
+?>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>ZINZINFALSE</title>
-    <meta name="ZINZINFALSE">
-    <script type="text/javascript" src="lib/phaser.js"></script>
-    <script type="text/javascript" src="src/Tableau1.js"></script>
-    <script type="text/javascript" src="src/Tuto.js"></script>
-    <script type="text/javascript" src="src/main.js"></script>
-    <link rel="stylesheet" href="src/cssphaser.css">
-</head>
-<body>
-<div id="scores">...</div>
-<div id="game"></div>
-</body>
-</html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ZINZINFALSE</title>
+        <meta name="ZINZINFALSE">
+        <script type="text/javascript" src="lib/phaser.js"></script>
+        <script type="text/javascript" src="src/Tableau1.js"></script>
+        <script type="text/javascript" src="src/Tuto.js"></script>
+        <script type="text/javascript" src="src/main.js"></script>
+        <link rel="stylesheet" href="src/cssphaser.css">
+    </head>
+        <body>
+            <div id="scores"><?php if (isset($_SESSION['score'])) {echo $_SESSION['score'] ;} else echo 0 ?></div>
+            <div id="game"></div>
+        </body>
+
 </html>
